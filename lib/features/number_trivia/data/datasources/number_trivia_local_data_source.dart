f3fb883201bacfdccda8b1f11d3fcfb0cb124f09
Sora DBH <47997682+duonghuan2122000@users.noreply.github.com>
2020-01-09 16:29:14 +0700

    local data source

--- lib/features/number_trivia/data/datasources/number_trivia_local_data_source.dart
+++ lib/features/number_trivia/data/datasources/number_trivia_local_data_source.dart
@@ -1,6 +1,11 @@
-import 'package:clean_architecture_tdd/features/number_trivia/data/models/number_trivia_model.dart';
+import 'dart:convert';
 
-abstract class NumberTriviaLocalDataSource{
+import 'package:clean_architecture_tdd/core/error/exceptions.dart';
+import 'package:clean_architecture_tdd/features/number_trivia/data/models/number_trivia_model.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:meta/meta.dart';
+
+abstract class NumberTriviaLocalDataSource {
   /**
    * * lấy bộ nhớ đệm [NumberTriviaModel] đã được lưu ở lần cuối
    * * Người dùng đã kết nối Internet
@@ -8,4 +13,30 @@ abstract class NumberTriviaLocalDataSource{
    */
   Future<NumberTriviaModel> getLastNumberTrivia();
   Future<void> cacheNumberTrivia(NumberTriviaModel triviaToCache);
+}
+
+const CACHED_NUMBER_TRIVIA = 'CACHED_NUMBER_TRIVIA';
+
+class NumberTriviaLocalDataSourceImpl implements NumberTriviaLocalDataSource {
+  final SharedPreferences sharedPreferences;
+
+  NumberTriviaLocalDataSourceImpl({@required this.sharedPreferences});
+
+  @override
+  Future<void> cacheNumberTrivia(NumberTriviaModel triviaToCache) {
+    // TODO: implement cacheNumberTrivia
+    return sharedPreferences.setString(
+        CACHED_NUMBER_TRIVIA, json.encode(triviaToCache.toJson()));
+  }
+
+  @override
+  Future<NumberTriviaModel> getLastNumberTrivia() {
+    // TODO: implement getLastNumberTrivia
+    final jsonString = sharedPreferences.getString(CACHED_NUMBER_TRIVIA);
+    if (jsonString != null) {
+      return Future.value(NumberTriviaModel.fromJson(json.decode(jsonString)));
+    } else {
+      throw CacheException();
+    }
+  }
 }

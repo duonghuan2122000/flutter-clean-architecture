0695e5351fdb8203771a6cac3ce2e99df29eb6f8
Sora DBH <47997682+duonghuan2122000@users.noreply.github.com>
2020-01-09 15:30:20 +0700

    network info

---
+++ test/core/network/network_info_test.dart
@@ -0,0 +1,33 @@
+import 'package:clean_architecture_tdd/core/network/network_info.dart';
+import 'package:mockito/mockito.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:data_connection_checker/data_connection_checker.dart';
+
+class MockDataConnectionChecker extends Mock
+  implements DataConnectionChecker {}
+
+void main(){
+  NetworkInfoImpl networkInfoImpl;
+  MockDataConnectionChecker mockDataConnectionChecker;
+
+  setUp((){
+    mockDataConnectionChecker = MockDataConnectionChecker();
+    networkInfoImpl = NetworkInfoImpl(mockDataConnectionChecker);
+  });
+
+  group('isConnected', (){
+    test(
+      'should forward the call to DataConnectionChecker.hasConnection',
+      () async{
+        //arrange
+        when(mockDataConnectionChecker.hasConnection)
+          .thenAnswer((_) async => true);
+        //act
+        final result = await networkInfoImpl.isConnected;
+        //assert
+        verify(mockDataConnectionChecker.hasConnection);
+        expect(result, true);
+      }
+    );
+  });
+}

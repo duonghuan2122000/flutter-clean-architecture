e2bf5ff0162dc94cbb9f5d05f60d32952e3b6f0d
Sora DBH <47997682+duonghuan2122000@users.noreply.github.com>
2020-01-11 16:01:32 +0700

    remote data source

--- lib/features/number_trivia/data/datasources/number_trivia_remote_data_source.dart
+++ lib/features/number_trivia/data/datasources/number_trivia_remote_data_source.dart
@@ -1,5 +1,9 @@
-import 'package:clean_architecture_tdd/features/number_trivia/data/models/number_trivia_model.dart';
+import 'dart:convert';
 
+import 'package:clean_architecture_tdd/core/error/exceptions.dart';
+import 'package:clean_architecture_tdd/features/number_trivia/data/models/number_trivia_model.dart';
+import 'package:meta/meta.dart';
+import 'package:http/http.dart' as http;
 abstract class NumberTriviaRemoteDataSource{
   /**
    * * Gọi đến http://numbersapi.com/{number}
@@ -12,4 +16,42 @@ abstract class NumberTriviaRemoteDataSource{
    * * Throws một [ServerException] cho tất cả lỗi.
    */
   Future<NumberTriviaModel> getRandomNumberTrivia();
+}
+
+class NumberTriviaRemoteDataSourceImpl implements NumberTriviaRemoteDataSource{
+  final http.Client client;
+
+  NumberTriviaRemoteDataSourceImpl({@required this.client});
+  @override
+  Future<NumberTriviaModel> getConcreteNumberTrivia(int number) async{
+    // TODO: implement getConcreteNumberTrivia
+    final response = await client.get(
+      'http://numbersapi.com/$number',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    );
+    if(response.statusCode == 200){
+      return NumberTriviaModel.fromJson(json.decode(response.body));
+    } else {
+      throw ServerException();
+    }
+  }
+
+  @override
+  Future<NumberTriviaModel> getRandomNumberTrivia() async{
+    // TODO: implement getRandomNumberTrivia
+    final response = await client.get(
+      'http://numbersapi.com/random',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    );
+    if(response.statusCode == 200){
+      return NumberTriviaModel.fromJson(json.decode(response.body));
+    } else {
+      throw ServerException();
+    }
+  }
+
 }
